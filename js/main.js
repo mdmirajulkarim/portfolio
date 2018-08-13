06a7d5abe69336043e26060d5c6358f39f7cf2a1
Sneakzz <sneakygamer94@gmail.com>
2018-08-13 22:21:07 +0200

    Implemented smooth scrolling and refactored existing js code.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,28 @@
 const setup = () => {
-  console.log('test');
+  smoothScrolling();
+}
 
-  /* Smooth scrolling proof of concept | NEEDS REFACTORING */
-  var link = document.getElementsByClassName('nav-link')[2];
-  console.log(link);
-  var dest = document.querySelector('#portfolio h2');
+const smoothScrolling = () => {
+  let aboutLink = document.getElementsByClassName('nav-link')[1];  
+  let projectsLink = document.getElementsByClassName('nav-link')[2];
+  let about = document.querySelector('#main #about');
+  let portfolio = document.querySelector('#portfolio h2');
           
-  link.addEventListener('click', e => {
+  aboutLink.addEventListener('click', e => {
     e.preventDefault();
-    dest.scrollIntoView({
-      behavior: 'smooth'
+    about.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
     });
-  });   
+  });
+
+  projectsLink.addEventListener('click', e => {
+    e.preventDefault();
+    portfolio.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
 }
 
 window.addEventListener('load', setup);
